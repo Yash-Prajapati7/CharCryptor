2850f8dada7a1c1405eb9ea39e45ec0895745282
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-07-27 17:23:17 +0530

    improved encryption logic for numbers in CharCryptor.java

--- CharCryptor.java
+++ CharCryptor.java
@@ -6,18 +6,30 @@ import java.util.Random;
 public class CharCryptor {
     private StringBuilder encryptedText;
     private int[] encryptionKey;
+    private int[] digitEncryptionKey;
 
     public CharCryptor(String text) {
         this.encryptedText = new StringBuilder(text);
         this.encryptionKey = generateEncryptionKey(text.length());
+        this.digitEncryptionKey = generateDigitEncryptionKey(text.length());
     }
 
-    // Generate a random encryption key
+    // Generate a random encryption key for letters
     private int[] generateEncryptionKey(int length) {
         int[] key = new int[length];
         Random random = new Random();
         for (int i = 0; i < length; i++) {
             key[i] = random.nextInt(26);  // Limiting the shift to 26 (number of letters in the alphabet)
+        }
+        return key;
+    }
+
+    // Generate a random encryption key for digits
+    private int[] generateDigitEncryptionKey(int length) {
+        int[] key = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            key[i] = random.nextInt(10);  // Limiting the shift to 10 (number of digits)
         }
         return key;
     }
@@ -38,7 +50,7 @@ public class CharCryptor {
         }
     }
 
-    // Shift characters forward in the alphabet and handle integer encryption
+    // Shift characters forward in the alphabet
     private char shiftCharForward(char c, int shift) {
         if (Character.isLowerCase(c)) {
             return (char) ((c - 'a' + shift) % 26 + 'a');
@@ -49,13 +61,13 @@ public class CharCryptor {
         }
     }
 
-    // Shift characters backward in the alphabet and handle integer decryption
+    // Shift characters backward in the alphabet
     private char shiftCharBackward(char c, int shift) {
         if (Character.isLowerCase(c)) {
             return (char) ((c - 'a' - shift + 26) % 26 + 'a');
         } else if (Character.isUpperCase(c)) {
             return (char) ((c - 'A' - shift + 26) % 26 + 'A');
-        } else {
+        } else { // github : Yash-Prajapati7
             return c; // Leave non-alphabetic characters unchanged
         }
     }
@@ -64,10 +76,11 @@ public class CharCryptor {
     private void encryptIntegers() {
         StringBuilder encryptedString = new StringBuilder();
 
-        for (char c : encryptedText.toString().toCharArray()) {
+        for (int i = 0; i < encryptedText.length(); i++) {
+            char c = encryptedText.charAt(i);
             if (Character.isDigit(c)) {
-                // Encrypt digit using a shifting operation (for example, shifting by 1)
-                char encryptedDigit = (char) (((c - '0') + 1) % 10 + '0');
+                // Encrypt digit using a shifting operation
+                char encryptedDigit = (char) (((c - '0') + digitEncryptionKey[i]) % 10 + '0');
                 encryptedString.append(encryptedDigit);
             } else {
                 encryptedString.append(c);
@@ -80,10 +93,11 @@ public class CharCryptor {
     private void decryptIntegers() {
         StringBuilder decryptedString = new StringBuilder();
 
-        for (char c : encryptedText.toString().toCharArray()) {
+        for (int i = 0; i < encryptedText.length(); i++) {
+            char c = encryptedText.charAt(i);
             if (Character.isDigit(c)) {
-                // Decrypt digit using a shifting operation (shifting back by 1)
-                char decryptedDigit = (char) (((c - '0') - 1 + 10) % 10 + '0');
+                // Decrypt digit using a shifting operation
+                char decryptedDigit = (char) (((c - '0') - digitEncryptionKey[i] + 10) % 10 + '0');
                 decryptedString.append(decryptedDigit);
             } else {
                 decryptedString.append(c);
